0b5dbebfd4764c1ecd22cc941522761f03cf6bd6
Nicola Asuni <nicolaasuni@users.noreply.github.com>
2018-06-27 11:48:16 +0100

    Test all languages in Travis CI and test C++ compatibility (#3)

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name='farmhash64',
-    version='2.0.0',
+    version='2.0.1',
     keywords=('farmhash64'),
     description="farmhash64 Bindings for Python",
     long_description=read('../README.md'),
