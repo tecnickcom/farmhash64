9564e848f32307474b8069ad5d7e0cd3f75fd8ff
nicolaasuni <info@tecnick.com>
2018-10-14 13:16:08 +0100

    Update golang and fix Python version

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name='farmhash64',
-    version='2.1.3',
+    version='2.1.4',
     keywords=('farmhash64'),
     description="farmhash64 Bindings for Python",
     long_description=read('../README.md'),
