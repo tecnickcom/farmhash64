87ffb72990c17b73adbaa0563c41fd0cf10e910b
nicolaasuni <info@tecnick.com>
2024-02-22 15:58:32 +0000

    Add go linter rules and adjust code accordingly

--- cgo/src/farmhash64.go
+++ cgo/src/farmhash64.go
@@ -48,19 +48,25 @@ import "unsafe"
 // FarmHash64 returns a 64-bit fingerprint hash for a string.
 func FarmHash64(s []byte) uint64 {
 	slen := len(s)
+
 	var p unsafe.Pointer
+
 	if slen > 0 {
 		p = unsafe.Pointer(&s[0]) /* #nosec */
 	}
+
 	return uint64(C.farmhash64((*C.char)(p), C.size_t(slen)))
 }
 
 // FarmHash32 returns a 32-bit fingerprint hash for a string.
 func FarmHash32(s []byte) uint32 {
 	slen := len(s)
+
 	var p unsafe.Pointer
+
 	if slen > 0 {
 		p = unsafe.Pointer(&s[0]) /* #nosec */
 	}
+
 	return uint32(C.farmhash32((*C.char)(p), C.size_t(slen)))
 }
--- go/src/farmhash64.go
+++ go/src/farmhash64.go
@@ -34,13 +34,17 @@ package farmhash64
 // BASICS
 
 // Some primes between 2^63 and 2^64 for various uses.
-const k0 uint64 = 0xc3a5c85c97cb3127
-const k1 uint64 = 0xb492b66fbe98f273
-const k2 uint64 = 0x9ae16a3b2f90404f
+const (
+	k0 uint64 = 0xc3a5c85c97cb3127
+	k1 uint64 = 0xb492b66fbe98f273
+	k2 uint64 = 0x9ae16a3b2f90404f
+)
 
 // Magic numbers for 32-bit hashing.  Copied from Murmur3.
-const c1 uint32 = 0xcc9e2d51
-const c2 uint32 = 0x1b873593
+const (
+	c1 uint32 = 0xcc9e2d51
+	c2 uint32 = 0x1b873593
+)
 
 type uint128 struct {
 	lo uint64
@@ -79,10 +83,11 @@ func mur(a, h uint32) uint32 {
 	a *= c2
 	h ^= a
 	h = rotate32(h, 19)
+
 	return h*5 + 0xe6546b64
 }
 
-// Merge a 64 bit integer into 32 bit
+// Merge a 64 bit integer into 32 bit.
 func mix64To32(x uint64) uint32 {
 	return mur(uint32(x>>32), uint32((x<<32)>>32))
 }
@@ -94,33 +99,40 @@ func hashLen16Mul(u, v, mul uint64) uint64 {
 	b := (v ^ a) * mul
 	b ^= (b >> 47)
 	b *= mul
+
 	return b
 }
 
 func hashLen0to16(s []byte) uint64 {
 	slen := uint64(len(s))
+
 	if slen >= 8 {
 		mul := k2 + slen*2
 		a := fetch64(s, 0) + k2
 		b := fetch64(s, int(slen-8))
 		c := rotate64(b, 37)*mul + a
 		d := (rotate64(a, 25) + b) * mul
+
 		return hashLen16Mul(c, d, mul)
 	}
 
 	if slen >= 4 {
 		mul := k2 + slen*2
 		a := fetch32(s, 0)
+
 		return hashLen16Mul(slen+(uint64(a)<<3), uint64(fetch32(s, int(slen-4))), mul)
 	}
+
 	if slen > 0 {
 		a := s[0]
 		b := s[slen>>1]
 		c := s[slen-1]
 		y := uint32(a) + (uint32(b) << 8)
 		z := uint32(slen) + (uint32(c) << 2)
+
 		return shiftMix(uint64(y)*k2^uint64(z)*k0) * k2
 	}
+
 	return k2
 }
 
@@ -133,6 +145,7 @@ func hashLen17to32(s []byte) uint64 {
 	b := fetch64(s, 8)
 	c := fetch64(s, slen-8) * mul
 	d := fetch64(s, slen-16) * k2
+
 	return hashLen16Mul(rotate64(a+b, 43)+rotate64(c, 30)+d, a+rotate64(b+k2, 18)+c, mul)
 }
 
@@ -145,6 +158,7 @@ func weakHashLen32WithSeedsWords(w, x, y, z, a, b uint64) (uint64, uint64) {
 	a += x
 	a += y
 	b += rotate64(a, 44)
+
 	return a + z, b + c
 }
 
@@ -172,22 +186,28 @@ func hashLen33to64(s []byte) uint64 {
 	f := fetch64(s, 24)
 	g := (y + fetch64(s, slen-32)) * mul
 	h := (z + fetch64(s, slen-24)) * mul
+
 	return hashLen16Mul(rotate64(e+f, 43)+rotate64(g, 30)+h, e+rotate64(f+a, 18)+g, mul)
 }
 
 // FarmHash64 returns a 64-bit fingerprint hash for a string.
 func FarmHash64(s []byte) uint64 {
 	slen := len(s)
+
 	var seed uint64 = 81
+
 	if slen <= 16 {
 		return hashLen0to16(s)
 	}
+
 	if slen <= 32 {
 		return hashLen17to32(s)
 	}
+
 	if slen <= 64 {
 		return hashLen33to64(s)
 	}
+
 	// For strings over 64 bytes we loop.
 	// Internal state consists of 56 bytes: v, w, x, y, and z.
 	v := uint128{0, 0}
@@ -199,6 +219,7 @@ func FarmHash64(s []byte) uint64 {
 	endIdx := ((slen - 1) / 64) * 64
 	last64Idx := endIdx + ((slen - 1) & 63) - 63
 	last64 := s[last64Idx:]
+
 	for len(s) > 64 {
 		x = rotate64(x+y+v.lo+fetch64(s, 8), 37) * k1
 		y = rotate64(y+v.hi+fetch64(s, 48), 42) * k1
@@ -210,6 +231,7 @@ func FarmHash64(s []byte) uint64 {
 		x, z = z, x
 		s = s[64:]
 	}
+
 	mul := k1 + ((z & 0xff) << 1)
 	// Make s point to the last 64 bytes of input.
 	s = last64
@@ -224,6 +246,7 @@ func FarmHash64(s []byte) uint64 {
 	v.lo, v.hi = weakHashLen32WithSeeds(s, v.hi*mul, x+w.lo)
 	w.lo, w.hi = weakHashLen32WithSeeds(s[32:], z+w.hi, y+fetch64(s, 16))
 	x, z = z, x
+
 	return hashLen16Mul(hashLen16Mul(v.lo, w.lo, mul)+shiftMix(y)*k0+z, hashLen16Mul(v.hi, w.hi, mul)+x, mul)
 }
 
