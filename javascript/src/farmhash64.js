a33de2ca5ba300e38c4a22cc958dd11458e0a85a
nicolaasuni <info@tecnick.com>
2024-03-12 16:44:42 +0000

    Draft Javascript

--- javascript/src/farmhash64.js
+++ javascript/src/farmhash64.js
@@ -28,58 +28,83 @@
 
 const k0 = {
     hi: 0xc3a5c85c,
-    lo: 0x97cb3127
+    lo: 0x97cb3127,
 };
 const k1 = {
     hi: 0xb492b66f,
-    lo: 0xbe98f273
+    lo: 0xbe98f273,
 };
 const k2 = {
     hi: 0x9ae16a3b,
-    lo: 0x2f90404f
+    lo: 0x2f90404f,
 };
 
 const c1 = 0xcc9e2d51;
 const c2 = 0x1b873593;
 
 function u64Add(a, b) {
-    const overflow =
+    const rst =
         ((a.lo & 0x7fffffff) + (b.lo & 0x7fffffff)) >>> 0 > 0x7fffffff ? 1 : 0;
     return {
-        hi: (a.hi + b.hi + overflow) >>> 0,
+        hi: (a.hi + b.hi + rst) >>> 0,
         lo: (a.lo + b.lo) >>> 0,
     };
 }
 
-function u64Mul(a, b) {
-    const a00 = a.lo & 0xffff;
-    const a16 = a.lo >>> 16;
-    const b00 = b.lo & 0xffff;
-    const b16 = b.lo >>> 16;
-    const a0b0 = a00 * b00;
-    const a0b16 = a00 * b16;
-    const a16b0 = a16 * b00;
-    const a16b16 = a16 * b16;
-    const lo = a0b0 + (a0b16 << 16) + (a16b0 << 16);
-    const hi = a.hi * b.hi + a16b16 + a0b16 + a16b0 + (lo >>> 16);
+function u32Split16(a) {
     return {
-        hi: hi,
-        lo: lo,
+        hi: ((a >>> 16) & 0xffff) >>> 0,
+        lo: (a & 0xffff) >>> 0,
     };
+}
+
+function u32Mul(a, b) {
+    const x = u32Split16(a);
+    const y = u32Split16(b);
+    return (x.lo * y.lo + (((x.hi * y.lo + x.lo * y.hi) << 16) >>> 0)) >>> 0;
+}
+
+function u32Mul64(a, b) {
+    const x = u32Split16(a);
+    const y = u32Split16(b);
+    const s = {
+        hi: u32Mul(x.hi, y.hi),
+        lo: u32Mul(x.lo, y.lo)
+    };
+    const t = u32Mul(x.hi, y.lo);
+    const u = u32Mul(x.lo, y.hi);
+    const v = {
+        hi: t >>> 16,
+        lo: (t << 16) >>> 0,
+    };
+    const w = {
+        hi: u >>> 16,
+        lo: (u << 16) >>> 0,
+    };
+    return u64Add(u64Add(s, v), w);
+}
+
+function u64Mul(a, b) {
+    return u64Add({
+            hi: (u32Mul(a.hi, b.lo) + u32Mul(a.lo, b.hi)) >>> 0,
+            lo: 0,
+        },
+        u32Mul64(a.lo, b.lo)
+    );
 }
 
 function u32RotR(a, s) {
     if (s <= 0 || s >= 32) {
         return a >>> 0;
     }
-    return (a >>> s) | (a << (32 - s));
+    return (a << (32 - s)) | (a >>> s);
 }
 
 function u64RotR(a, s) {
     if (s <= 0 || s >= 64) {
         return {
-            hi: a.hi,
-            lo: a.lo,
+            hi: a.hi >>> 0,
+            lo: a.lo >>> 0,
         };
     }
     if (s < 32) {
@@ -90,8 +115,8 @@ function u64RotR(a, s) {
     }
     if (s === 32) {
         return {
-            hi: a.lo,
-            lo: a.hi,
+            hi: a.lo >>> 0,
+            lo: a.hi >>> 0,
         };
     }
     return {
@@ -103,8 +128,8 @@ function u64RotR(a, s) {
 function u64ShiftL(a, s) {
     if (s <= 0 || s >= 64) {
         return {
-            hi: a.hi,
-            lo: a.lo,
+            hi: a.hi >>> 0,
+            lo: a.lo >>> 0,
         };
     }
     if (s < 32) {
@@ -122,8 +147,8 @@ function u64ShiftL(a, s) {
 function u64ShiftR(a, s) {
     if (s <= 0 || s >= 64) {
         return {
-            hi: a.hi,
-            lo: a.lo,
+            hi: a.hi >>> 0,
+            lo: a.lo >>> 0,
         };
     }
     if (s < 32) {
@@ -140,8 +165,8 @@ function u64ShiftR(a, s) {
 
 function u64XOR(a, b) {
     return {
-        hi: a.hi ^ b.hi,
-        lo: a.lo ^ b.lo,
+        hi: (a.hi ^ b.hi) >>> 0,
+        lo: (a.lo ^ b.lo) >>> 0,
     };
 }
 
@@ -204,48 +229,87 @@ function hashLen16Mul(u, v, mul) {
 function hashLen0to16(s) {
     slen = s.length;
     if (slen >= 8) {
-        mul = u64Add(k2, u64Mul(slen, {
-            hi: 0,
-            lo: 2
-        }));
-        a = u64Add(fetch64(s, 0), k2);
-        b = fetch64(s, slen - 8);
-        c = u64Add(u64Mul(rotate64(b, 37), mul), a);
-        d = u64Mul(u64Add(rotate64(a, 25), b), mul);
+        mul = u64Add(
+            k2,
+            u64Mul(slen, {
+                hi: 0,
+                lo: 2,
+            })
+        );
+        a = u64Add(fetchU64(s, 0), k2);
+        b = fetchU64(s, slen - 8);
+        c = u64Add(u64Mul(u64RotR(b, 37), mul), a);
+        d = u64Mul(u64Add(u64RotR(a, 25), b), mul);
         return hashLen16Mul(c, d, mul);
     }
     if (slen >= 4) {
-        mul = u64Add(k2, u64Mul(slen, {
-            hi: 0,
-            lo: 2
-        }));
-        a = fetch32(s, 0);
+        mul = u64Add(
+            k2,
+            u64Mul(slen, {
+                hi: 0,
+                lo: 2,
+            })
+        );
+        a = fetchU32(s, 0);
         return hashLen16Mul(
             u64Add(slen, u64ShiftL(a, 3)),
-            fetch32(s, slen - 4),
+            fetchU32(s, slen - 4),
             mul
         );
     }
 
     if (slen > 0) {
-        a = s.charCodeAt(0);
-        b = s.charCodeAt(slen >>> 1);
-        c = s.charCodeAt(slen - 1);
-        y = a + (b << 8);
-        z = slen + (c << 2);
+        a = s.charCodeAt(0) >>> 0;
+        b = s.charCodeAt(slen >>> 1) >>> 0;
+        c = s.charCodeAt(slen - 1) >>> 0;
+        y = (a + (b << 8)) >>> 0;
+        z = (slen + (c << 2)) >>> 0;
 
+
+        yk2 = u64Mul({
+                hi: 0,
+                lo: y,
+            },
+            k2
+        );
+
+        zk0 = u64Mul({
+                hi: 0,
+                lo: z,
+            },
+            k0
+        );
+
+        xor = u64XOR(yk2, zk0);
+
+        sm = shiftMix(xor);
+
+        console.error(s, toString(yk2), toString(zk0), toString(xor), toString(sm)); //DEBUG
+
+        return u64Mul(sm, k2);
+
+
+        /*
         return u64Mul(
             shiftMix(
-                u64XOR(u64Mul({
-                    hi: 0,
-                    lo: y
-                }, k2), u64Mul({
-                    hi: 0,
-                    lo: z
-                }, k0))
+                u64XOR(
+                    u64Mul({
+                            hi: 0,
+                            lo: y,
+                        },
+                        k2
+                    ),
+                    u64Mul({
+                            hi: 0,
+                            lo: z,
+                        },
+                        k0
+                    )
+                )
             ),
             k2
         );
+        */
     }
 
     return k2;
@@ -253,51 +317,57 @@ function hashLen0to16(s) {
 
 function hashLen17to32(s) {
     slen = s.length;
-    mul = u64Add(k2, u64Mul(slen, {
-        hi: 0,
-        lo: 2
-    }));
-    a = u64Mul(fetch64(s, 0), k1);
-    b = fetch64(s, 8);
-    c = u64Mul(fetch64(s, slen - 8), mul);
-    d = u64Mul(fetch64(s, slen - 16), k2);
+    mul = u64Add(
+        k2,
+        u64Mul(slen, {
+            hi: 0,
+            lo: 2,
+        })
+    );
+    a = u64Mul(fetchU64(s, 0), k1);
+    b = fetchU64(s, 8);
+    c = u64Mul(fetchU64(s, slen - 8), mul);
+    d = u64Mul(fetchU64(s, slen - 16), k2);
     return hashLen16Mul(
-        u64Add(u64Add(rotate64(u64Add(a, b), 43), rotate64(c, 30)), d),
-        u64Add(u64Add(a, rotate64(u64Add(b, k2), 18)), c),
+        u64Add(u64Add(u64RotR(u64Add(a, b), 43), u64RotR(c, 30)), d),
+        u64Add(u64Add(a, u64RotR(u64Add(b, k2), 18)), c),
         mul
     );
 }
 
 function hashLen33to64(s) {
     slen = s.length;
-    mul = u64Add(k2, u64Mul(slen, {
-        hi: 0,
-        lo: 2
-    }));
-    a = u64Add(fetch64(s, 0), k2);
-    b = fetch64(s, 8);
-    c = u64Mul(fetch64(s, slen - 8), mul);
-    d = u64Mul(fetch64(s, slen - 16), k2);
-    y = u64Add(u64Add(rotate64(u64Add(a, b), 43), rotate64(c, 30)), d);
-    z = hashLen16Mul(y, u64Add(u64Add(a, rotate64(u64Add(b, k2), 18)), c), mul);
-    e = u64Mul(fetch64(s, 16), mul);
-    f = fetch64(s, 24);
-    g = u64Add(y, u64Mul(fetch64(s, slen - 32)), mul);
-    h = u64Add(z, u64Mul(fetch64(s, slen - 24)), mul);
+    mul = u64Add(
+        k2,
+        u64Mul(slen, {
+            hi: 0,
+            lo: 2,
+        })
+    );
+    a = u64Add(fetchU64(s, 0), k2);
+    b = fetchU64(s, 8);
+    c = u64Mul(fetchU64(s, slen - 8), mul);
+    d = u64Mul(fetchU64(s, slen - 16), k2);
+    y = u64Add(u64Add(u64RotR(u64Add(a, b), 43), u64RotR(c, 30)), d);
+    z = hashLen16Mul(y, u64Add(u64Add(a, u64RotR(u64Add(b, k2), 18)), c), mul);
+    e = u64Mul(fetchU64(s, 16), mul);
+    f = fetchU64(s, 24);
+    g = u64Add(y, u64Mul(fetchU64(s, slen - 32), mul));
+    h = u64Add(z, u64Mul(fetchU64(s, slen - 24), mul));
     return hashLen16Mul(
-        u64Add(u64Add(rotate64(u64Add(e, f), 43), rotate64(g, 30)), h),
-        u64Add(u64Add(e, rotate64(u64Add(f, a), 18)), g),
+        u64Add(u64Add(u64RotR(u64Add(e, f), 43), u64RotR(g, 30)), h),
+        u64Add(u64Add(e, u64RotR(u64Add(f, a), 18)), g),
         mul
     );
 }
 
 function weakHashLen32WithSeedsWords(w, x, y, z, a, b) {
     a = u64Add(a, w);
-    b = rotate64(u64Add(u64Add(b, a), z), 21);
+    b = u64RotR(u64Add(u64Add(b, a), z), 21);
     c = a;
     a = u64Add(a, x);
     a = u64Add(a, y);
-    b = u64Add(b, rotate64(a, 44));
+    b = u64Add(b, u64RotR(a, 44));
     return {
         hi: u64Add(b, c),
         lo: u64Add(a, z),
@@ -306,10 +376,10 @@ function weakHashLen32WithSeedsWords(w, x, y, z, a, b) {
 
 function weakHashLen32WithSeeds(s, idx, a, b) {
     return weakHashLen32WithSeedsWords(
-        fetch64(s, idx + 0),
-        fetch64(s, idx + 8),
-        fetch64(s, idx + 16),
-        fetch64(s, idx + 24),
+        fetchU64(s, idx + 0),
+        fetchU64(s, idx + 8),
+        fetchU64(s, idx + 16),
+        fetchU64(s, idx + 24),
         a,
         b
     );
@@ -332,7 +402,15 @@ function farmhash64(s) {
         return hashLen33to64(s);
     }
 
-    x = u64Add(u64Mul(seed, k2), fetch64(s, 0));
+    v = {
+        hi: 0,
+        lo: 0,
+    };
+    w = {
+        hi: 0,
+        lo: 0,
+    };
+    x = u64Add(u64Mul(seed, k2), fetchU64(s, 0));
     y = u64Add(u64Mul(seed, k1), 113);
     z = u64Mul(shiftMix(u64Add(u64Mul(y, k2), 113)), k2);
 
@@ -342,19 +420,19 @@ function farmhash64(s) {
 
     while (slen > 64) {
         x = u64Mul(
-            rotate64(u64Add(u64Add(u64Add(x, y), v.lo), fetch64(s, idx + 8)), 37),
+            u64RotR(u64Add(u64Add(u64Add(x, y), v.lo), fetchU64(s, idx + 8)), 37),
             k1
         );
-        y = u64Mul(rotate64(u64Add(u64Add(y, v.hi), fetch64(s, idx + 48)), 42), k1);
+        y = u64Mul(u64RotR(u64Add(u64Add(y, v.hi), fetchU64(s, idx + 48)), 42), k1);
         x = u64XOR(x, w.hi);
-        y = u64Add(y, u64Add(v.lo, fetch64(s, idx + 40)));
-        z = u64Mul(rotate64(u64Add(z, w.lo), 33), k1);
+        y = u64Add(y, u64Add(v.lo, fetchU64(s, idx + 40)));
+        z = u64Mul(u64RotR(u64Add(z, w.lo), 33), k1);
         v = weakHashLen32WithSeeds(s, idx, u64Mul(v.hi, k1), u64Add(x, w.lo));
         w = weakHashLen32WithSeeds(
             s,
             idx + 32,
             u64Add(z, w.hi),
-            u64Add(y, fetch64(s, idx + 16))
+            u64Add(y, fetchU64(s, idx + 16))
         );
         tmp = x;
         x = z;
@@ -365,7 +443,7 @@ function farmhash64(s) {
 
     mul = u64Add(k1, {
         hi: 0,
-        lo: ((z.lo & 0xff) << 1) >>> 0
+        lo: (((z.lo >>> 0) & 0xff) << 1) >>> 0,
     });
     idx = last64Idx;
     w.lo = u64Add(w.lo, {
@@ -375,19 +453,19 @@ function farmhash64(s) {
     v.lo = u64Add(v.lo, w.lo);
     w.lo = u64Add(w.lo, v.lo);
     x = u64Mul(
-        rotate64(u64Add(u64Add(u64Add(x, y), v.lo), fetch64(s, idx + 8)), 37),
+        u64RotR(u64Add(u64Add(u64Add(x, y), v.lo), fetchU64(s, idx + 8)), 37),
         mul
     );
-    y = u64Mul(rotate64(u64Add(u64Add(y, v.hi), fetch64(s, idx + 48)), 42), mul);
+    y = u64Mul(u64RotR(u64Add(u64Add(y, v.hi), fetchU64(s, idx + 48)), 42), mul);
     x = u64XOR(x, u64Mul(w.hi, 9));
-    y = u64Add(y, u64Add(u64Mul(v.lo, 9), fetch64(s, idx + 40)));
-    z = u64Mul(rotate64(u64Add(z, w.lo), 33), mul);
+    y = u64Add(y, u64Add(u64Mul(v.lo, 9), fetchU64(s, idx + 40)));
+    z = u64Mul(u64RotR(u64Add(z, w.lo), 33), mul);
     v = weakHashLen32WithSeeds(s, idx, u64Mul(v.hi, mul), u64Add(x, w.lo));
     w = weakHashLen32WithSeeds(
         s,
         idx + 32,
         u64Add(z, w.hi),
-        u64Add(y, fetch64(s, idx + 16))
+        u64Add(y, fetchU64(s, idx + 16))
     );
     tmp = x;
     x = z;
@@ -425,5 +503,7 @@ if (typeof module !== "undefined") {
         farmhash64: farmhash64,
         parseHex: parseHex,
         toString: toString,
+
+        u32Mul: u32Mul,
     };
 }
--- javascript/test/test_farmhash64.js
+++ javascript/test/test_farmhash64.js
@@ -7,20 +7,264 @@
  * @link       https://github.com/tecnickcom/farmhash64
  */
 
-const { farmhash64, farmhash32, parseHex, toString } = require(process.argv[2]);
+const {
+    farmhash64,
+    farmhash32,
+    parseHex,
+    toString,
+} = require(process.argv[2]);
+
+var test_data = [
+    [0xfe0061e9, {
+        hi: 0x9ae16a3b,
+        lo: 0x2f90404f
+    }, "9ae16a3b2f90404f", ""],
+    [0xd824662a, {
+        hi: 0xb3454265,
+        lo: 0xb6df75e3
+    }, "b3454265b6df75e3", "a"],
+    [0x15eb5ed6, {
+        hi: 0xaa8d6e52,
+        lo: 0x42ada51e
+    }, "aa8d6e5242ada51e", "ab"],
+    [0xcaf25fe2, {
+        hi: 0x24a5b3a0,
+        lo: 0x74e7f369
+    }, "24a5b3a074e7f369", "abc"],
+    [0xcf297808, {
+        hi: 0x1a5502de,
+        lo: 0x4a1f8101
+    }, "1a5502de4a1f8101", "abcd"],
+    [0x5f8d48db, {
+        hi: 0xc22f4663,
+        lo: 0xe54e04d4
+    }, "c22f4663e54e04d4", "abcde"],
+    [0x16b8a2fd, {
+        hi: 0xc329379e,
+        lo: 0x6a03c2cd
+    }, "c329379e6a03c2cd", "abcdef"],
+    [0xcfc5f43d, {
+        hi: 0x3c40c92b,
+        lo: 0x1ccb7355
+    }, "3c40c92b1ccb7355", "abcdefg"],
+    [0x08d1b642, {
+        hi: 0xfee9d229,
+        lo: 0x90c82909
+    }, "fee9d22990c82909", "abcdefgh"],
+    [0xb382832e, {
+        hi: 0x332c8ed4,
+        lo: 0xdae5ba42
+    }, "332c8ed4dae5ba42", "abcdefghi"],
+    [0x3f19a3cb, {
+        hi: 0xad052244,
+        lo: 0xb781c4eb
+    }, "ad052244b781c4eb", "0123456789"],
+    [0x0ee83c5c, {
+        hi: 0x3ef4c035,
+        lo: 0x14208c77
+    }, "3ef4c03514208c77", "0123456789 "],
+    [0x6fca023f, {
+        hi: 0x496841e8,
+        lo: 0x3a33cc91
+    }, "496841e83a33cc91", "0123456789-0"],
+    [0x6b2c02bd, {
+        hi: 0xd81bcb9f,
+        lo: 0x3679ac0c
+    }, "d81bcb9f3679ac0c", "0123456789~01"],
+    [0x0b8e8fba, {
+        hi: 0x5da5a6a1,
+        lo: 0x17c606f6
+    }, "5da5a6a117c606f6", "0123456789#012"],
+    [0xe6946835, {
+        hi: 0x5361eae1,
+        lo: 0x7c1ff6bc
+    }, "5361eae17c1ff6bc", "0123456789@0123"],
+    [0xfa44df74, {
+        hi: 0x4283d4ef,
+        lo: 0x43627f64
+    }, "4283d4ef43627f64", "0123456789'01234"],
+    [0x2a1ed264, {
+        hi: 0x46a7416e,
+        lo: 0xd4861e3b
+    }, "46a7416ed4861e3b", "0123456789=012345"],
+    [0xbcd3277f, {
+        hi: 0xa4abb4e0,
+        lo: 0xda2c594c
+    }, "a4abb4e0da2c594c", "0123456789+0123456"],
+    [0x26bf5a67, {
+        hi: 0xcf1c7d3a,
+        lo: 0xd54f9215
+    }, "cf1c7d3ad54f9215", "0123456789*01234567"],
+    [0x8eedb634, {
+        hi: 0x07adf50b,
+        lo: 0x2ac764fc
+    }, "07adf50b2ac764fc", "0123456789&012345678"],
+    [0xa329652e, {
+        hi: 0xdebcba8e,
+        lo: 0x6f3eabd1
+    }, "debcba8e6f3eabd1", "0123456789^0123456789"],
+    [0x4ba9b4ed, {
+        hi: 0x4dbd128a,
+        lo: 0xf51d77e8
+    }, "4dbd128af51d77e8", "0123456789%0123456789£"],
+    [0x1b9ea72f, {
+        hi: 0xd78d5f85,
+        lo: 0x2d522e6a
+    }, "d78d5f852d522e6a", "0123456789$0123456789!0"],
+    [0x819d77a5, {
+        hi: 0x80d73b84,
+        lo: 0x3ba57db8
+    }, "80d73b843ba57db8", "size:  a.out:  bad magic"],
+    [0x8b72761e, {
+        hi: 0x8eb3808d,
+        lo: 0x1ccfc779
+    }, "8eb3808d1ccfc779", "Nepal premier won't resign."],
+    [0x5f21fe43, {
+        hi: 0xb944f8a1,
+        lo: 0x6261e414
+    }, "b944f8a16261e414", "C is as portable as Stonehedge!!"],
+    [0xa15ead04, {
+        hi: 0xe8f89ab6,
+        lo: 0xdf9bdd25
+    }, "e8f89ab6df9bdd25", "Discard medicine more than two years old."],
+    [0xe3763baf, {
+        hi: 0xa9961670,
+        lo: 0xce2a46d9
+    }, "a9961670ce2a46d9", "I wouldn't marry him with a ten foot pole."],
+    [0x50a48aaa, {
+        hi: 0xbdd69b79,
+        lo: 0x8d6ba37a
+    }, "bdd69b798d6ba37a", "If the enemy is within range, then so are you."],
+    [0x517e346c, {
+        hi: 0xc2f8db86,
+        lo: 0x24fefc0e
+    }, "c2f8db8624fefc0e", "The major problem is with sendmail.  -Mark Horton"],
+    [0x8a4b0b6c, {
+        hi: 0x5a0a6efd,
+        lo: 0x52e84e2a
+    }, "5a0a6efd52e84e2a", "How can you write a big system without C++?  -Paul Glick"],
+    [0xb360937b, {
+        hi: 0x786d7e19,
+        lo: 0x87023ca9
+    }, "786d7e1987023ca9", "He who has a shady past knows that nice guys finish last."],
+    [0x2e5713b3, {
+        hi: 0x5d14f96c,
+        lo: 0x18fe3d5e
+    }, "5d14f96c18fe3d5e", "Free! Free!/A trip/to Mars/for 900/empty jars/Burma Shave"],
+    [0xec6d1e0e, {
+        hi: 0xec8848fd,
+        lo: 0x3b266c10
+    }, "ec8848fd3b266c10", "His money is twice tainted: 'taint yours and 'taint mine."],
+    [0x7175f31d, {
+        hi: 0x2a578b80,
+        lo: 0xbb82147c
+    }, "2a578b80bb82147c", "The days of the digital watch are numbered.  -Tom Stoppard"],
+    [0xdf4c5297, {
+        hi: 0x55182f88,
+        lo: 0x59eca4ce
+    }, "55182f8859eca4ce", "For every action there is an equal and opposite government program."],
+    [0x62359aca, {
+        hi: 0xabcdb319,
+        lo: 0xfcf2826c
+    }, "abcdb319fcf2826c", "You remind me of a TV show, but that's all right: I watch it anyway."],
+    [0x398c0b7c, {
+        hi: 0x1d857025,
+        lo: 0x03ac7eb4
+    }, "1d85702503ac7eb4", "It's well we cannot hear the screams/That we create in others' dreams."],
+    [0x00047f9c, {
+        hi: 0xa2b8bf30,
+        lo: 0x32021993
+    }, "a2b8bf3032021993", "Give me a rock, paper and scissors and I will move the world.  CCFestoon"],
+    [0xe56239a7, {
+        hi: 0x38aa3175,
+        lo: 0xb37f305c
+    }, "38aa3175b37f305c", "It's a tiny change to the code and not completely disgusting. - Bob Manchek"],
+    [0xb556f325, {
+        hi: 0x7e85d7b0,
+        lo: 0x50ed2967
+    }, "7e85d7b050ed2967", "There is no reason for any individual to have a computer in their home. -Ken Olsen, 1977"],
+    [0x75cc5362, {
+        hi: 0x5a05644e,
+        lo: 0xb66e435e
+    }, "5a05644eb66e435e", "Even if I could be Shakespeare, I think I should still choose to be Faraday. - A. Huxley"],
+    [0xc401a0bf, {
+        hi: 0x098eff69,
+        lo: 0x58c5e91a
+    }, "098eff6958c5e91a", "The fugacity of a constituent in a mixture of gases at a given temperature is proportional to its mole fraction.  Lewis-Randall Rule"],
+    [0x4e56b7e9, {
+        hi: 0xc3f02c4f,
+        lo: 0xfd5d71e6
+    }, "c3f02c4ffd5d71e6", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
+];
+
+
 
 // @TODO ...
 
+
+
+function test_parseHex() {
+    var errors = 0;
+    var i;
+    var hn;
+    var exp = {
+        "hi": 0x12345678,
+        "lo": 0x90abcdef
+    };
+    hn = parseHex("1234567890AbCdEf");
+    if ((hn.hi != exp.hi) || (hn.lo != exp.lo)) {
+        console.error("parseHex: expected ", exp, ", got ", hn);
+        ++errors;
+    }
+    for (i = 0; i < test_data.length; i++) {
+        hn = parseHex(test_data[i][2]);
+        if ((hn.hi != test_data[i][1].hi) || (hn.lo != test_data[i][1].lo)) {
+            console.error("parseHex: (" + i + ") expected ", toString(test_data[i][1]), ", got ", toString(hn));
+            ++errors;
+        }
+    }
+    return errors;
+}
+
+function test_toString() {
+    var errors = 0;
+    var hs = "";
+    var i;
+    for (i = 0; i < test_data.length; i++) {
+        hs = toString(test_data[i][1]);
+        if (hs !== test_data[i][2]) {
+            console.error("toString: (" + i + ") expected ", test_data[i][2], ", got ", hs);
+            ++errors;
+        }
+    }
+    return errors;
+}
+
+function test_farmhash64() {
+    var errors = 0;
+    var h = 0;
+    var i = 0;
+    for (i = 0; i < test_data.length; i++) {
+        h = farmhash64(test_data[i][3]);
+        if ((h.hi != test_data[i][1].hi) || (h.lo != test_data[i][1].lo)) {
+            console.error("farmhash64: (" + i + ") expected " + toString(test_data[i][1]) + " but got " + toString(h));
+            ++errors;
+        }
+    }
+    return errors;
+}
+
+
 var errors = 0;
 
-//gentestmap();
-
-//errors += test_farmhash64();
+errors += test_parseHex();
+errors += test_toString();
+errors += test_farmhash64();
 //errors += test_farmhash32();
 
 if (errors > 0) {
-  console.log("FAILED: " + errors);
-  process.exit(1);
+    console.log("FAILED: " + errors);
+    process.exit(1);
 } else {
-  console.log("OK");
+    console.log("OK");
 }
