45dd053b66d8fd81896c4c102b4200ea40f7c936
nicolaasuni <info@tecnick.com>
2024-03-15 11:46:35 +0000

    Enable CodeQL workflow

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/tecnickcom/farmhash64
 
 go 1.22
 
+toolchain go1.22.1
+
 require (
 	github.com/jstemmer/go-junit-report v1.0.0
 	github.com/rakyll/gotest v0.0.6
--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="farmhash64",
-    version="1.8.1.0",
+    version="1.8.2.0",
     keywords=("farmhash64"),
     description="farmhash64 Bindings for Python",
     long_description=read("../README.md"),
