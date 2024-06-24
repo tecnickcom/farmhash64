721f89922b56edece3cdd1e4a490626128d3bbff
nicolaasuni <info@tecnick.com>
2024-06-24 10:33:02 +0100

    Updat edependencies

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="farmhash64",
-    version="1.9.10.0",
+    version="1.9.11.0",
     keywords=("farmhash64"),
     description="farmhash64 Bindings for Python",
     long_description=read("../README.md"),
