83b662444b13dfc794570476d084c16883c8ff0a
nicolaasuni <info@tecnick.com>
2024-08-27 12:13:23 +0100

    Bump version

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="farmhash64",
-    version="1.9.15.0",
+    version="1.9.16.0",
     keywords=("farmhash64"),
     description="farmhash64 Bindings for Python",
     long_description=read("../README.md"),
