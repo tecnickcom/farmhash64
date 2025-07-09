2500752484fca6e0bb1cc305ff497e6b7a538bc4
nicolaasuni <info@tecnick.com>
2025-07-09 06:22:15 +0100

    Update dependencies

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/tecnickcom/farmhash64
 
 go 1.24
 
-toolchain go1.24.4
+toolchain go1.24.5
 
 require (
 	github.com/jstemmer/go-junit-report/v2 v2.1.0
--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="farmhash64",
-    version="1.9.43.0",
+    version="1.9.44.0",
     keywords=("farmhash64"),
     description="farmhash64 Bindings for Python",
     long_description=read("../README.md"),
