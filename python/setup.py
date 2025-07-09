029af340f2e5d379fefc48bdcfc918cc136040ad
nicolaasuni <info@tecnick.com>
2025-07-09 06:44:04 +0100

    Fix minor warnings

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="farmhash64",
-    version="1.9.44.0",
+    version="1.9.45.0",
     keywords=("farmhash64"),
     description="farmhash64 Bindings for Python",
     long_description=read("../README.md"),
@@ -55,6 +55,7 @@ setup(
                 "-Wunused-value",
                 "-Wcast-align",
                 "-Wundef",
+                "-Wformat",
                 "-Wformat-security",
                 "-Wshadow",
                 "-Wno-format-overflow",
