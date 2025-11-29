9bd046ec408afa2c1cc6343e79d1c624077da1ca
Nicola Asuni <info@tecnick.com>
2025-11-29 07:32:11 +0000

    update dependencies

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/tecnickcom/farmhash64
 
 go 1.25
 
-toolchain go1.25.3
+toolchain go1.25.4
--- php/test/FarmHash64Test.php
+++ php/test/FarmHash64Test.php
@@ -1,5 +1,7 @@
 <?php
 
+// @phpcs:disable Generic.Files.LineLength
+
 namespace Test;
 
 use PHPUnit\Framework\TestCase;
