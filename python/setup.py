c2e0811c536817d79b3a414ab90ca6121a6c5cf7
Nicola Asuni <info@tecnick.com>
2026-07-04 17:57:54 +0100

    chore: update dependencies

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="farmhash64",
-    version="1.9.73.0",
+    version="1.9.74.0",
     keywords=("farmhash64"),
     description="farmhash64 Bindings for Python",
     long_description=read("../README.md"),
