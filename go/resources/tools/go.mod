43e8e92e5db5c695aa2013fe59059574eaf67c7f
Nicola Asuni <info@tecnick.com>
2026-09-07 18:49:50 +0100

    build: add Go toolchain

--- go/resources/tools/go.mod
+++ go/resources/tools/go.mod
@@ -5,6 +5,8 @@ module github.com/tecnickcom/farmhash64/go/resources/tools
 
 go 1.24
 
+toolchain go1.27.1
+
 tool github.com/jstemmer/go-junit-report/v2
 
 require github.com/jstemmer/go-junit-report/v2 v2.1.0 // indirect
--- python/setup.py
+++ python/setup.py
@@ -35,7 +35,7 @@ class RunTests(Command):
 
 setup(
     name="farmhash64",
-    version="1.10.4.0",
+    version="1.10.5.0",
     keywords=("farmhash64"),
     description="farmhash64 Bindings for Python",
     long_description=read("README.md"),
