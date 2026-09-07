6d4d7e27b6f1ae1649c5ab2a5a8cccc75db54a94
Nicola Asuni <info@tecnick.com>
2026-09-07 16:21:18 +0100

    build: lower Go requirement to 1.24

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/tecnickcom/farmhash64
 
-go 1.27
+go 1.24
 
 toolchain go1.27.1
--- python/setup.py
+++ python/setup.py
@@ -35,7 +35,7 @@ class RunTests(Command):
 
 setup(
     name="farmhash64",
-    version="1.10.2.0",
+    version="1.10.3.0",
     keywords=("farmhash64"),
     description="farmhash64 Bindings for Python",
     long_description=read("README.md"),
